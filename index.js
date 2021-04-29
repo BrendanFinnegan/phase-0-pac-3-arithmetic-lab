565645956615c086a6afbb7e8c646dca50d0e58c
BrendanFinnegan <bf289907@gmail.com>
2021-04-29 16:05:25 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+let add = (a, b) => {
+    return a + b
+}
+let subtract = (a, b) => {
+    return a - b
+}
+
+let multiply = (a, b) => {
+    return a * b
+}
+let divide = (a, b) => {
+    return a/b
+}
+
+let increment = a => {
+    a++;
+   return a
+}
+let decrement = a => {
+    a--;
+    return a
+ }
+ let makeInt = n => {
+     return parseInt(n, 10);
+     
+ }
+ let preserveDecimal = z => {
+return parseFloat(z);
+
+
+ }
